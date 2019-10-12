09c5d300895e112408bd0beba1b0681706edb743
wrk1231 <wrk1231@gmail.com>
2019-10-11 23:59:43 -0400

    Backtest module init commit

--- BackTest.py
+++ BackTest.py
@@ -18,4 +18,25 @@ class BACKTEST(object):
     def __init__(self):
         self.MY_API_KEY = 'J4PS1W9LI8IL0E97'
         self.ONEYEARDAYS = 252
+        self.cov_begin_date = datetime(2005, 1, 3).date()
+        self.ret_begin_date = datetime(2006, 1, 3).date()
 
+    def get_daily_close(self, symbol, for_cov=False):
+        full_data, meta_data = ts.get_daily(symbol=symbol, outputsize='full')
+        series_index = pd.Series(full_data.index)
+        full_data.index = series_index.apply(lambda t: np.datetime64(t).astype(datetime).date()).values
+        assert '4. close' in full_data.columns
+        close = full_data['4. close']
+        close.index.name = 'Date'
+        close.name = symbol
+        assert close.index[0] < datetime(2005, 1, 3).date()
+        if for_cov:
+            return close[self.cov_begin_date:]
+        else:
+            return close[self.ret_begin_date:]
+
+class ERBACKTEST(BACKTEST):
+    pass
+
+class ECBACKTEST(BACKTEST):
+    pass
